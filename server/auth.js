79ad5b0244d3e6a7b7e933a4d282c2e29db4ddcd
Vu Phung <vphung5707@gmail.com>
2017-10-17 11:30:14 -0700

    oauth google schema setup.

---
+++ server/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'googleAuth' : {
+        'clientId' : '314930734797-1vphas9ccttkbtkjsqog639dmouotu4n.apps.googleusercontent.com',
+        'clientSecret' : 'sq83IC94-W1dTPcO_ah--KmG',
+        'callbackURL' : 'http://localhost:3000/auth/google/callback'
+    }
+}
