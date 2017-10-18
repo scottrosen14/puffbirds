de6f474cc2dbd9f77940c6b0c404aefb409feb58
scottrosen14 <scott.rosen14@gmail.com>
2017-10-18 14:47:30 -0700

    nothing

--- server/server.js
+++ server/server.js
@@ -52,12 +52,13 @@ app.put('/api/cal', calendarController.updateRow);
 app.delete('/api/cal', calendarController.removeRow);
 
 
-
 const server = app.listen(3000, function () {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
 
 // socket.io stuff
 const io = socket(server);
